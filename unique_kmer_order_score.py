e71719dd5695ed54009257112ac40dbbde716e12
cchu70 <cchu70@gatech.edu>
2019-07-08 16:20:12 -0400

    changed out loop

--- unique_kmer_order_score.py
+++ unique_kmer_order_score.py
@@ -13,8 +13,8 @@ def main():
 	mashmap = sys.argv[2]
 
 
-	# For each read, find the corresponding alignments *(array of tuples)
-	alignments = parseMashMap(mashmap)
+	# For each read, get the indices of the unique kmers
+	kmer_indices = parseDump(dump)
 
 	with open(mashmap, "r") as mh:
 
@@ -23,63 +23,41 @@ def main():
 			read_name = data[0] 
 			start = int(data[7])
 			end = int(data[8])
-			aRange = (start, end, line.strip())
-			try: 
-				alignments[read_name].append(aRange)
-			except:
-				alignments[read_name] = [aRange]
-			#####
+			
+			score = scoreMashMapAlignments(read_name, start, end, kmer_indices[read_name])
+			print("%s\t%d" % (line.strip(), score))
 		#####
 
 		# Reached end of file
-		scoreMashMapAlignments(ref_indices, read_name, alignments[read_name])		
+		score = scoreMashMapAlignments(ref_indices, read_name, alignments[read_name])		
+		print("%s\t%d" % (line.strip(), score))
 #####
 
-def parseMashMap(mashmap_filename):
-	alignments = {}
-	with open(mashmap_filename, "r") as mh:
-		for line in mh:
-			data = line.split()
-			read_name = data[0] 
-			start = int(data[7])
-			end = int(data[8])
-			aRange = (start, end, line.strip())
-			try: 
-				alignments[read_name].append(aRange)
-			except:
-				alignments[read_name] = [aRange]
+def scoreMashMapAlignments(read_name, start, end, read_kmer_idx):
+	# Go through each alignment of the previous read
+	score = 0
+	begin = False
+	ref_idx_prev = 0
+
+	for ref_idx in read_kmer_idx:
+
+		if (ref_idx >= start and ref_idx <= end):
+			if (not begin):
+				# Intitialize
+				ref_idx_prev = ref_idx
+				begin = True
+			else:
+				if (ref_idx > ref_idx_prev):
+					score = score + 1
+				#####
+
+				# Update
+				ref_idx_prev = ref_idx
 			#####
 		#####
 	#####
-	return alignments
-#####
-
-def scoreMashMapAlignments(ref_indices, read_name, read_alignments):
-	# Go through each alignment of the previous read
-	for start, end, mashmap_info in read_alignments:
-		score = 0
-		ref_idx_prev= 0
-		begin = False
-
-		for ref_idx in ref_indices:
-
-			if (ref_idx >= start and ref_idx <= end):
-				if (not begin):
-					# Intitialize
-					ref_idx_prev = ref_idx
-					begin = True
-				else:
-					if (ref_idx > ref_idx_prev):
-						score = score + 1
-					#####
-
-					# Update
-					ref_idx_prev = ref_idx
-				#####
-			#####
-
-		#####
-		print("%s\t%d" % (mashmap_info, score))
+	return score
+	
 #####
 
 def parseDump(dump_filename):
