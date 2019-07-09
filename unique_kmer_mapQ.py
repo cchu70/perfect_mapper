d610934d500d344b42913b77b1436df18f91c1e8
cchu70 <cchu70@gatech.edu>
2019-07-09 19:16:06 -0400

    testing for second best

--- unique_kmer_mapQ.py
+++ unique_kmer_mapQ.py
@@ -84,8 +84,8 @@ def main():
 	#####
 
 	for read_name in alignments:
-		print(read_name)
 		if (alignments[read_name].second_best):
+			print(read_name)
 			print("First best: " % alignments[read_name].best.sck_count)
 			print("Second best: " % alignments[read_name].second_best.sck_count)
 
