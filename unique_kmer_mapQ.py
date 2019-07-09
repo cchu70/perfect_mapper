f5ddae7e2ba93c93de17db9ef283ddc0118f85ea
cchu70 <cchu70@gatech.edu>
2019-07-09 19:02:15 -0400

    weird ascii issues with code

--- unique_kmer_mapQ.py
+++ unique_kmer_mapQ.py
@@ -1,6 +1,5 @@
 # This script calculates the mapQ based on the same procedure as minimap2 (Heng Li), but uses the number of shared kmers
-
-# mapQ = 1 – f2/f1
+# mapQsck = 1 - f2/f1
 # Where fi is the number of shared unique kemrs between a read and its alignment i. For each read, we only consider the first (f1) and second best (f2)
 import sys
 import os.path
@@ -60,7 +59,7 @@ def main:
 	map_sck_counts = sys.argv[1]
 
 	if (not os.path.isfile(map_sck_counts)):
-		sys.stderr.write("%s does nto exist" % map_sck_counts)
+		sys.stderr.write("%s does not exist" % map_sck_counts)
 		assert false
 	#####
 
