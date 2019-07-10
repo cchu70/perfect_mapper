edb039de100754e31e94e327a95c88e4ff2c9094
cchu70 <cchu70@gatech.edu>
2019-07-10 16:43:35 -0400

    refactor names

--- unique_kmer_mapQ.py
+++ unique_kmer_mapQ.py
@@ -8,45 +8,45 @@ import math
 
 class TopTwo:
 
-	best = None
-	second_best = None
+	# best = None
+	# second_best = None
 
-	def add(self, align_data):
-		# print(align_data.data)
-		if (self.best == None):
-			self.best = align_data
+	# def add(self, align_data):
+	# 	# print(align_data.data)
+	# 	if (self.best == None):
+	# 		self.best = align_data
 
-		elif (align_data.greaterThan(self.best)):
-			# print("Original best %s" % self.best.sck_count)
-			self.second_best = self.best
-			self.best = align_data
-			# print("New best: %d" % align_data.sck_count)
-		elif (align_data.greaterThan(self.second_best)):
-			self.second_best = align_data
-			#print("Second best: %d" % align_data.sck_count)
-		#####
-	#####
+	# 	elif (align_data.greaterThan(self.best)):
+	# 		# print("Original best %s" % self.best.sck_count)
+	# 		self.second_best = self.best
+	# 		self.best = align_data
+	# 		# print("New best: %d" % align_data.sck_count)
+	# 	elif (align_data.greaterThan(self.second_best)):
+	# 		self.second_best = align_data
+	# 		#print("Second best: %d" % align_data.sck_count)
+	# 	#####
+	# #####
 
-	def __init__(self, align_data_A, align_data_B=None):
+	# def __init__(self, align_data_A, align_data_B=None):
 
-		if (align_data_A.greaterThan(align_data_B)):
-			self.best = align_data_A
-			self.second_best = align_data_B
-		else:
-			self.best = align_data_B
-			self.second_best = align_data_A
-		#####
-		# print("Best: %s\n sck_count %s" % (self.best.data, self.best.sck_count))\
+	# 	if (align_data_A.greaterThan(align_data_B)):
+	# 		self.best = align_data_A
+	# 		self.second_best = align_data_B
+	# 	else:
+	# 		self.best = align_data_B
+	# 		self.second_best = align_data_A
+	# 	#####
+	# 	# print("Best: %s\n sck_count %s" % (self.best.data, self.best.sck_count))\
 
-	def mapQScore(self):
-		if (self.second_best != None):
-			if (self.best.sck_count > 0):
-				self.best.score =  1.0 - (float(self.second_best.sck_count)/float(self.best.sck_count))
-			else:
-				self.best.score = 0.0
-		else:
-			self.best.score =  1.0
-		#####
+	# def mapQScore(self):
+	# 	if (self.second_best != None):
+	# 		if (self.best.sck_count > 0):
+	# 			self.best.score =  1.0 - (float(self.second_best.sck_count)/float(self.best.sck_count))
+	# 		else:
+	# 			self.best.score = 0.0
+	# 	else:
+	# 		self.best.score =  1.0
+	# 	#####
 #####
 
 class ReadAlignments:
@@ -70,13 +70,13 @@ class ReadAlignments:
 	def add_align(self, align_data):
 		if (align_data.is_primary):
 			if (self.primary):
-				self.primary.incr_sck_count(align_data.sck_count)
+				self.primary.incr_shared_sck_count(align_data.shared_sck_count)
 			else:
 				self.primary = align_data
 			#####
 		else:
 			if (self.secondary):
-				self.secondary.incr_sck_count(align_data.sck_count)
+				self.secondary.incr_shared_sck_count(align_data.shared_sck_count)
 			else:
 				self.secondary = align_data
 			#####
@@ -86,11 +86,11 @@ class ReadAlignments:
 	def mapQScore(self):
 		# 40 * (1 - f2/f1) * min(1, m/200) * log f1
 
-		if (self.primary.sck_count == 0):
+		if (self.primary.shared_sck_count == 0):
 			print("Prim has no unique kmers")
 			self.score =  0
 		elif (self.secondary):
-			self.score =  40 * (1 - self.secondary.sck_count / self.primary.sck_count) * min (1.0, self.primary.order_score / 200)
+			self.score =  40 * (1 - self.secondary.shared_sck_count / self.primary.shared_sck_count) * min (1.0, self.primary.order_score / 200)
 			print("Secondary also has kmers")
 			print(self.secondary)
 		else:
@@ -109,7 +109,7 @@ class ReadAlignments:
 
 class AlignData:
 	read_name = ""
-	sck_count = 0
+	shared_sck_count = 0
 	order_score = 0
 	MQ = 0
 
@@ -128,20 +128,20 @@ class AlignData:
 		if (align_data == None):
 			return True
 		else:
-			return self.sck_count > align_data.sck_count 
+			return self.shared_sck_count > align_data.shared_sck_count 
 		#####
 	#####
 
-	def set_sck_count(self,i):
-		self.sck_count = i
+	def set_shared_sck_count(self,i):
+		self.shared_sck_count = i
 	#####
 
-	def incr_sck_count(self, to_add):
-		self.sck_count = self.sck_count + to_add
+	def incr_shared_sck_count(self, to_add):
+		self.shared_sck_count = self.shared_sck_count + to_add
 	#####
 
 	def __str__(self):
-		return "%s\t%0.5f" % (self.data, self.sck_count)
+		return "%s\t%0.5f" % (self.data, self.shared_sck_count)
 #####
 
 
@@ -161,9 +161,9 @@ class Minimap2Alignment(AlignData):
 		self.end_idx = float(data[4])
 		self.MQ = float(data[5])
 
-		self.sck_count = float(data[6])
+		self.shared_sck_count = float(data[6])
 		# self.order_score = float(data[3])
-		self.order_score = 100.0
+		self.order_score = float(data[8])
 		self.data = bam_string
 
 		
@@ -182,26 +182,26 @@ def main():
 
 	# Get the file with the read, the alignment, original mapping score, and the number of shared unique kmers
 
-	map_sck_counts = sys.argv[1]
+	map_shared_sck_counts = sys.argv[1]
 
-	if (not os.path.isfile(map_sck_counts)):
-		sys.stderr.write("%s does not exist\n" % map_sck_counts)
+	if (not os.path.isfile(map_shared_sck_counts)):
+		sys.stderr.write("%s does not exist\n" % map_shared_sck_counts)
 	else:
-		sys.stderr.write("Loading file %s\n" % map_sck_counts)
+		sys.stderr.write("Loading file %s\n" % map_shared_sck_counts)
 	#####
 	# read_name_idx = int(sys.argv[2])
-	# sck_count_idx = int(sys.argv[3])
+	# shared_sck_count_idx = int(sys.argv[3])
 	# order_score_idx = int(sys.argv[4])
 
 	
 	alignments = {}
 	# Collect all the shared unique kmers counts
 
-	for line in open(map_sck_counts, "r"):
+	for line in open(map_shared_sck_counts, "r"):
 		# Dictionary of reads maintains queue of the sck alignment scores for each read
 		# data = line.split()
 		# read_name = data[read_name_idx]
-		# sck_count = int(data[sck_count_idx])
+		# shared_sck_count = int(data[shared_sck_count_idx])
 		# order_score = 
 		align_data = Minimap2Alignment(line.strip())
 
