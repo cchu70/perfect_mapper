3f4ad4a18fdb5eab0672ef136fd1b38b9ea43d60
cchu70 <cchu70@gatech.edu>
2019-07-15 16:52:22 -0400

    script to get reads with multiple alignments into a single sam file

--- get_multiple_alignments_from_bam.py
+++ get_multiple_alignments_from_bam.py
@@ -3,36 +3,54 @@
 
 import sys
 
-		
-def parseSam(sam_string):
-	# Based on sam file
-	read_name = sam_string[0]
-	flag = int(sam_string[1])
-	start = int(sam_string[3])
-	MQ = int(sam_string[4])
-	cigar = sam_string[5]
 
-	return read_name, flag, start, MQ, cigar
+def isTrue(align_start, true_start, true_end):
+	length = true_end - true_start
+	#print(align_start, true_start, true_end)
+	# print ("%d < %d < %d ?" % ((true_start - length / 2), align_start, (true_start + length / 2)))
+	if ( (true_start - length / 2) <= align_start < (true_start + length / 2) ):
+		return True
+	#####
+	return False
 
 def main():
 
 	# Stdin for sam file
 	sam_file = sys.stdin
 
+	true_origin_bedfile = sys.argv[1] # bedfile for the true regions of the reads
+
+	true_origin_table = {}
+	for line in open(true_origin_bedfile, "r"):
+		read = line.split()[-1]
+		start = int(line.split()[1])
+		end = int(line.split()[2])
+		true_origin_table[read] = (start, end)
+	#####
+
+	print(true_origin_table)
+		
+
 	curr_read_name = ""
 	first_data = ""
 
 	for line in sam_file:
 		read_name = line.split()[0]
+
+		start = int(line.split()[3])
+		isCorrect = isTrue(start, true_origin_table[read_name][0], true_origin_table[read_name][1])
+
+		data = "%s\t%s" % (line.strip(), isCorrect)
+
 		if (read_name == curr_read_name):
 			if (second):
 				print(first_data)
 				second = False
 			#####
-			print(line.strip())
+			print(data)
 		else:
 			curr_read_name = read_name
-			first_data = line.strip()
+			first_data = data
 			second = True
 		#####
 	#####
