5ed9a1ed56a4be1c70e019caf9ee8c21b9f6b234
cchu70 <cchu70@gatech.edu>
2019-07-16 15:20:57 -0400

    print float

--- weighted_jaccard/weighted_jaccard_func.py
+++ weighted_jaccard/weighted_jaccard_func.py
@@ -155,7 +155,7 @@ class Alignment:
 	#####
 
 	def toString(self):
-		scores_string = "\t".join(["%s=%d" % (sch, self.scores[sch]) for sch in self.scores])
+		scores_string = "\t".join(["%s=%0.5f" % (sch, self.scores[sch]) for sch in self.scores])
 		return "%d\t%d\t%s\t%s" % (self.start_idx, self.end_idx, self.ground_truth, scores_string)
 	#####
 
