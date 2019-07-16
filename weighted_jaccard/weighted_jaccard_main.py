9b82369697df73227dc794258387ce434ab93a82
cchu70 <cchu70@gatech.edu>
2019-07-16 11:59:50 -0400

    checking seq types

--- weighted_jaccard/weighted_jaccard_main.py
+++ weighted_jaccard/weighted_jaccard_main.py
@@ -59,7 +59,7 @@ def main():
 			#####
 		#####
 		# Update to next read
-		print(read_records[read_name])
+		print(read_records[read_name].sequence)
 		assert False
 		curr_read = Read(read_name, getKmers(read_records[read_name].sequence, k_size))
 
