93bf64274303a2d050bc0ec413364a7f22966681
cchu70 <cchu70@gatech.edu>
2019-07-16 14:22:28 -0400

    testing schemes

--- weighted_jaccard/weighted_jaccard_main.py
+++ weighted_jaccard/weighted_jaccard_main.py
@@ -64,11 +64,7 @@ def main():
 
 
 		# Get the alignment region's kmers
-		print(start, end)
-		print(ref_record.seq[start:end])
-		assert False
 		ref_k_set = getKmers(ref_record.seq[start:end], k_size)
-		print(ref_k_set)
 
 		# score alignments with different weighting schemes
 		for sch in schemes:
