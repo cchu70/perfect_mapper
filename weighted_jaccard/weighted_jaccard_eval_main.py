443a6217b0e5b31672a08b781d82c30a3d73498d
cchu70 <cchu70@gatech.edu>
2019-07-19 13:05:56 -0400

    eval revert print

--- weighted_jaccard/weighted_jaccard_eval_main.py
+++ weighted_jaccard/weighted_jaccard_eval_main.py
@@ -61,7 +61,7 @@ def main():
 						else:
 							# increment TN
 							sch_test[scheme][1] += 1
-							print("%s\t%s\t%s\t%s\tTN" % (curr_read, scheme, a[0], a[1]))
+							#print("%s\t%s\t%s\t%s\tTN" % (curr_read, scheme, a[0], a[1]))
 							# print(a[0])
 							# print(a[1])
 						#####
@@ -69,7 +69,7 @@ def main():
 						if a[1].ground_truth == "True":
 							# Increment FP
 							sch_test[scheme][2] += 1
-							print("%s\t%s\t%s\t%s\tFP" % (curr_read, scheme, a[0], a[1]))
+							#print("%s\t%s\t%s\t%s\tFP" % (curr_read, scheme, a[0], a[1]))
 							# print(curr_read)
 							# print(curr_sch_scores[scheme])
 						else:
@@ -96,7 +96,7 @@ def main():
 	#####
 
 
-	assert False
+	#assert False
 	# # Print results
 	for sch in sch_test:
 		rates = sch_test[sch]
