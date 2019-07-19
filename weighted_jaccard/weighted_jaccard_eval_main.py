3ea68904df42c61521b83e2f63e112a910556dd3
cchu70 <cchu70@gatech.edu>
2019-07-19 09:20:24 -0400

    eval score parsing forgot int conversion

--- weighted_jaccard/weighted_jaccard_eval_main.py
+++ weighted_jaccard/weighted_jaccard_eval_main.py
@@ -14,8 +14,8 @@ def parseWJ(wj_str):
 	scores = data[4:]
 	scores_table = {}
 	for s in scores:
-		scheme, score = s.split("=")
-		scores_table[scheme] = score
+		scheme, score, equation = s.split("=")
+		scores_table[scheme] = int(score)
 	#####
 
 	return read_name, start, end, ground_truth, scores_table
