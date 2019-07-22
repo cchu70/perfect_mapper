e1570cd516845a2c3ce446d3d6eeca2fa5541af7
cchu70 <cchu70@gatech.edu>
2019-07-22 13:54:04 -0400

    scheme score script

--- weighted_jaccard/weighted_jaccard_scheme_score.py
+++ weighted_jaccard/weighted_jaccard_scheme_score.py
@@ -32,39 +32,18 @@ def main():
 
 	parse_align_file = align_file_parser[align_file_type]
 
-	for line in open(align_file, "r"):
+	for line in open(k_count_file, "r"):
 
-		read_name, length, ref_start, ref_end, ground_truth, read_start, read_end, map_truth = parse_align_file(line.strip())
-
+		read_name, map_truth, ref_start, ref_end, ground_truth, shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum = parse_align_file(line.strip())
 		alignment = Alignment(read_name, map_truth, ref_start, ref_end, ground_truth)
 
-
-		# Check which read (current or next) this alignment corresponds to 
-		if (curr_read_str):
-			if (read_name != curr_read_name):
-				# evaluate the curr read performance
-				curr_read_name = read_name
-				curr_read_str = read_records[read_name]
-			# else, continue using this read
-			#####
-		else:
-			# initialize
-			curr_read_name = read_name
-			curr_read_str = read_records[read_name]
-		#####
-
-		# Get the alignment region's kmers
-		ref_k_set = getKmers(ref_record[ref_start:ref_end], k_size)
-
 		# score alignments with different weighting schemes
-		shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum = counts(getKmers(curr_read_str[read_start:read_end], k_size), ref_k_set, unique_table)
-		print("%s\t%s" % (alignment, "\t".join([shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum])))
-		# for sch in schemes:
-		# 	x = weightJaccard(sch.non_unique_weight, sch.unique_weight, shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum)
-		# 	alignment.scores[sch] = x
-		# 	# alignment.scores[sch] = [shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum]
-		# #####
-		# print("%s\t%s" % (alignment.toString(), [shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum]))
+		for sch in schemes:
+			x = weightJaccard(sch.non_unique_weight, sch.unique_weight, shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum)
+			alignment.scores[sch] = x
+			# alignment.scores[sch] = [shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum]
+		#####
+		print("%s\t%s" % (alignment.toString(), [shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum]))
 	#####
 
 
