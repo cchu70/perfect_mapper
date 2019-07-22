e18e8a999a199dd1a66af1e476fc46108fbe8644
cchu70 <cchu70@gatech.edu>
2019-07-22 13:34:53 -0400

    just output kmer counts

--- weighted_jaccard/weighted_jaccard_main.py
+++ weighted_jaccard/weighted_jaccard_main.py
@@ -88,12 +88,13 @@ def main():
 
 		# score alignments with different weighting schemes
 		shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum = counts(getKmers(curr_read_str[read_start:read_end], k_size), ref_k_set, unique_table)
-		for sch in schemes:
-			x = weightJaccard(sch.non_unique_weight, sch.unique_weight, shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum)
-			alignment.scores[sch] = x
-			# alignment.scores[sch] = [shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum]
-		#####
-		print("%s\t%s" % (alignment.toString(), [shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum]))
+		print("%s\t%s" % (alignment, "\t".join([shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum])))
+		# for sch in schemes:
+		# 	x = weightJaccard(sch.non_unique_weight, sch.unique_weight, shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum)
+		# 	alignment.scores[sch] = x
+		# 	# alignment.scores[sch] = [shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum]
+		# #####
+		# print("%s\t%s" % (alignment.toString(), [shared_unique_sum, shared_non_unique_sum, non_shared_unique_sum, non_shared_non_unique_sum]))
 	#####
 
 
