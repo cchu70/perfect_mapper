bd056c617ede3d3843a422cb5ffede2e5d93b2bb
cchu70 <cchu70@gatech.edu>
2019-07-22 14:12:59 -0400

    input change for eval

--- weighted_jaccard/weighted_jaccard_func.py
+++ weighted_jaccard/weighted_jaccard_func.py
@@ -232,7 +232,7 @@ class Alignment:
 		self.scores[scheme] = score
 	#####
 
-	def toString(self):
+	def scoreString(self):
 		# scores_string = "\t".join(["(%0.3f, %0.3f)=%0.5f=%s" % (sch.non_unique_weight, sch.unique_weight, self.scores[sch][0],  self.scores[sch][1]) for sch in self.scores])
 		# return "%s\t%s\t%d\t%d\t%s\t%s" % (self.read_name, self.map_truth, self.start_idx, self.end_idx, self.ground_truth, scores_string)
 		# counts_str = ",".join(self.scores[sch])
