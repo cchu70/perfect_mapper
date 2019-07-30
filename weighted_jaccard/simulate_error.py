39c92122ca8e0d32d0f7de73cf28b225d7dfe33c
cchu70 <cchu70@gatech.edu>
2019-07-30 14:32:34 -0400

    typos

--- weighted_jaccard/simulate_error.py
+++ weighted_jaccard/simulate_error.py
@@ -20,13 +20,13 @@ def sim_err(error_rate, seq):
 
 	new_seq = ""
 
-	threshold = 100 * error_rate
+	threshold = int(100 * error_rate)
 	error_count = 0
 
 	for c in seq:
 		x = random.randint(0,101)
 
-		if x <= threshold :
+		if x % threshold == 0 :
 			# Do edit base
 			error_count += 1
 
