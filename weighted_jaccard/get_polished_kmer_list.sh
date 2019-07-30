6a8ff96955a1bc1bf7ad9980580a4426e3970ad6
cchu70 <cchu70@gatech.edu>
2019-07-30 10:34:32 -0400

    script to get kmers

---
+++ weighted_jaccard/get_polished_kmer_list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# script to make the uniqe kmer and true kmer list from the POLISHED version of the chrX assembly
+
+module load canu
+
+# make the db
+meryl count k=21 memory=40 threads=12 chrX.fasta output chrX.meryl
+
+# Dump
+meryl-lookup -dump -sequence chrX.fasta -mers chrX.meryl -t12 -m1 | awk '$5 >0 {print $4"\t"$5}'
