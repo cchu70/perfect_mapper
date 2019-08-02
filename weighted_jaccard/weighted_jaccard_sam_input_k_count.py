511524348dd48e6f338850b7d1ae7b3ceb02bc01
cchu70 <cchu70@gatech.edu>
2019-08-02 17:09:40 -0400

    hardcode input

--- weighted_jaccard/weighted_jaccard_sam_input_k_count.py
+++ weighted_jaccard/weighted_jaccard_sam_input_k_count.py
@@ -15,15 +15,22 @@ def run_count(err_str, prefix, ver, which_part):
 
 	line = ""
 	cmd = "python ../../scripts/mashmap_postfilter/weighted_jaccard/weighted_jaccard_count_plain_sam_input.py GAGE_%s.sim_reads.fasta error_%s/%s.err_%s_%s.v_%d.fasta error_%s/%s_minimap2.N50_r3k.split.err_%s_%s.v_%s.aligned_%s.sam GAGE.kmerlist.txt 21 GAGE_%s GAGE_%s %s" % (which_part, err_str, prefix, err_str, which_part, ver, err_str, prefix, err_str, which_part, ver, which_part, which_part, which_part, err_str)
-	print(cmd)
+	#print(cmd)
 	p1 = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 
 	output = p1.communicate()[0]
-	print(output)
+	#print(output)
 	return output
 
 
 def main():
+
+
+	errors = sys.argv[1]
+
+	errors = errors.split(",")
+
+	prefix = sys.argv[2]
 
 	errors = ['0.0', '0.0001', '0.0002', '0.0003', '0.0004', '0.0005', '0.0006', '0.0007', '0.0008', '0.0009']
 	# errors = ['0.0', '0.0001']
