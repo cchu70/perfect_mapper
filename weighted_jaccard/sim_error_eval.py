35b5012084954a4e9af33adbb354f0b8bbfea79e
cchu70 <cchu70@gatech.edu>
2019-08-07 14:39:12 -0400

    redo minimap eval

--- weighted_jaccard/sim_error_eval.py
+++ weighted_jaccard/sim_error_eval.py
@@ -28,20 +28,29 @@ def main():
 			# from_A_unaligned = int(data[6])
 			# from_B_unaligned = int(data[7])
 
+			A_correct = float(from_A_align_A)/ float(from_A_align_A + from_A_align_B)
+			B_correct = float(from_B_align_B)/float(from_B_align_B + from_B_align_A)
 
 
-			if which_err == "A":
-				p_turnover = float(from_A_align_B) / float(from_A_align_A + from_A_align_B)
-				p_remaining = float(from_A_align_A) / float(from_A_align_A + from_A_align_B)
-				# p_total_wrong = (float(from_A_align_B) + float(from_A_unaligned)) / float(from_A_align_A + from_A_align_B)
-			else:
-				p_turnover = float(from_B_align_A) / float(from_B_align_B + from_B_align_A)
-				p_remaining = float(from_B_align_B) / float(from_B_align_B + from_B_align_A)
-				# p_total_wrong = (float(from_B_align_B) + float(from_B_unaligned)) / float(from_B_align_B + from_B_align_A)
-			#####
+			to_print_A = "%0.8f\t%0.8f\t%0.8f\tGAGE_%s\t%s" % (err, A_correct, 0, which_err, "GAGE_A")
+			to_print_B = "%0.8f\t%0.8f\t%0.8f\tGAGE_%s\t%s" % (err, B_correct, 0, which_err, "GAGE_B")
+			print(to_print_A)
+			print(to_print_B)
+
+
+			# if which_err == "A":
+			# 	p_turnover = float(from_A_align_B) / float(from_A_align_A + from_A_align_B)
+			# 	p_remaining = float(from_A_align_A) / float(from_A_align_A + from_A_align_B)
+			# 	# p_total_wrong = (float(from_A_align_B) + float(from_A_unaligned)) / float(from_A_align_A + from_A_align_B)
+			# else:
+			# 	p_turnover = float(from_B_align_A) / float(from_B_align_B + from_B_align_A)
+			# 	p_remaining = float(from_B_align_B) / float(from_B_align_B + from_B_align_A)
+			# 	# p_total_wrong = (float(from_B_align_B) + float(from_B_unaligned)) / float(from_B_align_B + from_B_align_A)
+			# #####
+
 
 			# print("%0.8f\t%0.8f\t%0.8f\t%0.8f\t%s" % (err, p_turnover, p_remaining, p_total_wrong, which_err))
-			print("%0.8f\t%0.8f\t%0.8f\t%s" % (err, p_turnover, p_remaining, which_err))
+			# print("%0.8f\t%0.8f\t%0.8f\t%s" % (err, p_turnover, p_remaining, which_err))
 
 
 		#####
