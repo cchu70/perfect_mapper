1188f8970de1f2e428014979f198da615a5737d7
cchu70 <cchu70@gatech.edu>
2019-08-08 17:19:35 -0400

    folders

--- weighted_jaccard/plot_sim_error_minimap2.R
+++ weighted_jaccard/plot_sim_error_minimap2.R
@@ -11,14 +11,11 @@ table=args[2]			# Weighted Jaccard Performance Percentage output
 
 minimap2_perf=read.table(table, header=T)
 
+# Combine the combination of aligned reads and the part errored 
 minimap2_perf$Combination = paste(minimap2_perf$V4, minimap2_perf$V5)
 
 # Plot
 ggplot(data=minimap2_perf, aes(x=V1, y=V2, col=Combination)) + geom_point() + ylim(0,1) + xlab("Error rate introduced") + ylab("Retention Rate") + theme_bw()
 # Subset or change the coloring schemes to view different combinations
-
-# Examples
-# Plot only the performances of weighting schemes where the non-uniqmer weight is 0
-# ggplot(data=subset(vary_weights, (V6 == 0)), aes(x=V1, y=V2, col=paste(V3,V4))) + geom_point() + ylim(0,1) + theme_bw() + labs(fill = "Weight 0 for non-uniq-mers")
 
 ggsave(file=paste(out,'.plot_sim_error_minimap2.png', sep=""))
--- weighted_jaccard/weited_jaccard_count_plain_sam_input.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# script to run weighted_jaccard_count_plain_sam_input.py
-
-
-# need to pass in a lot of data to run .. 
